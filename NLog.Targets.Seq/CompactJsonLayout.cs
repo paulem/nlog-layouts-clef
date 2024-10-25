a127e2a98b5775d50d0882191c084d6ae58af843
Pavel Mikhaylov <pavel.um@gmail.com>
2024-10-25 14:54:37 +0300

    Align the code with the changes in NLog.Targets.Seq

--- NLog.Targets.Seq/CompactJsonLayout.cs
+++ NLog.Targets.Seq/CompactJsonLayout.cs
@@ -23,11 +23,15 @@ namespace NLog.Layouts.ClefJsonLayout
     public class CompactJsonLayout : JsonLayout
     {
         readonly JsonAttribute
-            _timestampAttribute = new JsonAttribute("@t", new SimpleLayout("${date:format=o}")),
-            _levelAttribute = new JsonAttribute("@l", new SimpleLayout("${level}")),
-            _exceptionAttribute = new JsonAttribute("@x", new SimpleLayout("${exception:format=toString}")),
-            _messageAttribute = new JsonAttribute("@m", new FormattedMessageLayout()),
-            _messageTemplateAttribute = new JsonAttribute("@mt", new SimpleLayout("${onhasproperties:${message:raw=true}}"));
+            _timestampAttribute = new("@t", new SimpleLayout("${date:format=o}")),
+            _levelAttribute = new("@l", new SimpleLayout("${level}")),
+            _exceptionAttribute = new("@x", new SimpleLayout("${exception:format=toString}")),
+            _messageAttribute = new("@m", new FormattedMessageLayout()),
+            _messageTemplateAttribute = new("@mt", new SimpleLayout("${onhasproperties:${message:raw=true}}")),
+            _traceIdAttribute = new("@tr", Layout.FromMethod(evt => System.Diagnostics.Activity.Current?.GetTraceId())),
+            _spanIdAttribute = new("@sp", Layout.FromMethod(evt => System.Diagnostics.Activity.Current?.GetSpanId()));
+        
+        public Layout LogLevel { get => _levelAttribute.Layout; set => _levelAttribute.Layout = value; }
 
         public CompactJsonLayout()
         {
@@ -38,7 +42,9 @@ namespace NLog.Layouts.ClefJsonLayout
             var renderingsAttribute = new JsonAttribute("@r", new RenderingsLayout(new Lazy<IJsonConverter>(ResolveService<IJsonConverter>)), encode: false);
             Attributes.Add(renderingsAttribute);
             Attributes.Add(_messageAttribute);
-
+            Attributes.Add(_traceIdAttribute);
+            Attributes.Add(_spanIdAttribute);
+            
             IncludeEventProperties = true;
             IncludeScopeProperties = true;
             SuppressSpaces = true;
